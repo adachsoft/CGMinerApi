882835d5fbf33d2b3e7757d0b56885dba402ab68
Arkadiusz Adach <adach_soft@wp4b.pl>
2018-01-16 07:36:01 +0100

    1

--- example.php
+++ example.php
@@ -1,12 +1,10 @@
 <?php
 require_once './src/CGMinerApi.php';
 
-echo "DIR: " . __DIR__ . "\r\n";
-
-
 if ($argc == 1) {
 	echo "php -f example.php <ip>\r\n";
 } else {
+	echo "IP: " . $argv[1] . "\r\n";
 	$cgminer = new CGMinerApi($argv[1]);
 	$cgminer->sendSummary();
 	$cgminer->printSummary();
