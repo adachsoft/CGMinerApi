99d838547d68360c0bd3585ad20d361fc146916c
Arkadiusz Adach <adach_soft@wp4b.pl>
2018-01-27 12:00:27 +0100

    Monitor class

--- src/Monitor.php
+++ src/Monitor.php
@@ -9,6 +9,9 @@ namespace AdachSoft\AntMiner;
 class Monitor
 {
 
+	const STATE_OK = 0;
+	const STATE_LOW_HASH_RATE = 1;
+	const STATE_REBOOTED = 2;
 	const HASH_RATE = [
 		'Antminer L3+' => 500,
 		'Antminer D3' => 18000
@@ -26,6 +29,8 @@ class Monitor
 	protected $password;
 	protected $antminer;
 	protected $cgminer;
+	protected $timeLowHashRate;
+	protected $timeReboot;
 
 	public function __construct($host, $username, $password)
 	{
@@ -50,26 +55,88 @@ class Monitor
 		$this->cgminer = new CGMinerApi($host);
 	}
 
+	public function monitor()
+	{
+		while (true) {
+			$this->check();
+			sleep(1);
+		}
+	}
+
 	public function check()
 	{
 		echo "Addr: " . $this->host . "\r\n";
 		echo $this->antminer->getMinerType() . ":\t";
 
-		$this->cgminer->sendSummary();
-		$resultSummary = $this->cgminer->getResultSummary();
+		if ($this->getState() === static::STATE_REBOOTED) {
+			$t = time() - $this->timeReboot;
+			echo "Waiting($t)...";
+		} else {
+			$this->cgminer->sendSummary();
+			$resultSummary = $this->cgminer->getResultSummary();
 
-		if (isset($resultSummary['SUMMARY'][0])) {
-			$val = $resultSummary['SUMMARY'][0];
-			echo round($val['GHS 5s'], 2) . "\t";
-			echo round($val['GHS av'], 2) . "\t";
-			echo "\r\n";
+			if (isset($resultSummary['SUMMARY'][0])) {
+				$val = $resultSummary['SUMMARY'][0];
+
+				$hashRate = $val['GHS 5s'];
+				$hashRateAVG = $val['GHS av'];
+				$percent = 0;
+				if ($this->isNumber($hashRate) && $this->isNumber($hashRateAVG)) {
+					$hashRate = (float) $hashRate;
+					$percent = round(($hashRate / $this->hashRateReset) * 100.00, 2);
+					if ($percent < 90) {
+						$this->onLowHashRate($hashRate, $hashRateAVG, $percent);
+					}
+				} else {
+					echo "ERROR\r\n";
+				}
+
+				echo round($hashRate, 2) . "\t";
+				echo round($hashRateAVG, 2) . "\t";
+				echo round($percent, 2) . '%' . "\t";
+				echo "\r\n";
+			}
 		}
-
-		//$this->cgminer->printSummary();
 	}
 
 	public function IsDevicesAllowed($minerType)
 	{
 		return in_array($minerType, static::DEVICES_ALLOWED);
+	}
+
+	private function onLowHashRate($hashRate, $hashRateAVG, $percent)
+	{
+		echo "Low hash rate\r\n";
+		if (empty($this->timeLowHashRate)) {
+			$this->timeLowHashRate = time();
+		} elseif (time() - $this->timeLowHashRate > 60) {
+			$this->timeLowHashRate = NULL;
+			$this->timeReboot = time();
+			echo "REBOOT\r\n";
+		}
+	}
+
+	/**
+	 * Get state
+	 * @return int
+	 */
+	private function getState()
+	{
+		if (!empty($this->timeReboot)) {
+			return static::STATE_REBOOTED;
+		} elseif (!empty($this->timeReboot)) {
+			return static::STATE_LOW_HASH_RATE;
+		}
+		return static::STATE_OK;
+	}
+
+	/**
+	 * Is number
+	 * @param string $val
+	 * @return bool
+	 */
+	private function isNumber($val)
+	{
+		return is_numeric($val);
 	}
 }
